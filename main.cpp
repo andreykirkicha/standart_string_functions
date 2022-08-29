133908b604e4ec36b8a1ae9a27178903cd6290b2
andreykirkicha <kirkicha.andrey@mail.ru>
2022-08-29 18:51:20 +0300

    #4 добавлено тестирование

--- main.cpp
+++ main.cpp
@@ -5,11 +5,17 @@
 int main()
 {
     const char str1[] = "I'm just a string";
-    char str2[] = "I'm not just a string, I'm something more";
+    char str2[300] = "I'm not just a string, I'm something more";
+    char test_str2[300] = "I'm not just a string, I'm something more";
     const char str3[] = " end of string";
 
     printf("STRLEN");
-    printf("\t length of \"%s\" is %d\n\n\n", str1, str_len(str1));
+    printf("\t length of \"%s\" is %d\n\n", str1, str_len(str1));
+
+    if (str_len(str1) == strlen(str1))
+        printf("\t OK\n\n\n");
+    else
+        printf("\t FAILED\n\n\n");
 
 
     printf("STRCPY");
@@ -18,20 +24,32 @@ int main()
     printf("\t dest:\t \"%s\"\n", str2);
     printf("\t\t src:\t \"%s\"\n\n", str1);
 
-    str_cpy(str2, str1);
-
     printf("\t after");
     printf("\t dest:\t \"%s\"\n\n\n", str2);
 
+    if (!strcmp(str_cpy(str2, str1), strcpy(test_str2, str1)))
+        printf("\t OK\n\n\n");
+    else
+        printf("\t FAILED\n\n\n");
 
     printf("STRCHR");
     printf("\t \"%s\"\n\n\n", str_chr(str1, 'u'));
+
+    if (str_chr(str1, 'u') == strchr(str1, 'u'))
+        printf("\t OK\n\n\n");
+    else
+        printf("\t FAILED\n\n\n");
 
 
     printf("STRCMP");
     printf("\t str1: \"%s\"\n", str1);
     printf("\t str2: \"%s\"\n", str2);
     printf("\t %d\n\n\n", str_cmp(str1, str2));
+
+    if (str_cmp(str1, str2) == strcmp(str1, test_str2))
+        printf("\t OK\n\n\n");
+    else
+        printf("\t FAILED\n\n\n");
 
 
     printf("STRCAT");
@@ -40,10 +58,14 @@ int main()
     printf("\t dest:\t \"%s\"\n", str2);
     printf("\t\t src:\t \"%s\"\n\n", str3);
 
-    str_cat(str2, str3);
-
     printf("\t after");
     printf("\t dest:\t \"%s\"\n", str2);
+
+    if (!strcmp(str_cat(str2, str3), strcat(test_str2, str3)))
+        printf("\t OK\n\n\n");
+    else
+        printf("\t FAILED\n\n\n");
+
 /*
     char str4[] = "I'm not just a string, I'm something more";
 
--- string_functions.hpp
+++ string_functions.hpp
@@ -1,24 +1,37 @@
 /*!
     Analog of strlen (<string.h>)
+    \param[in] str - string which length the function finds
+    \return length of given string
 */
 int str_len( const char *str );
 
 /*!
     Analog of strcpy (<string.h>)
+    \param[out] dest - destination string
+    \param[in] src - source string
+    \return string with copied data
 */
 char *str_cpy( char *dest, const char *src );
 
 /*!
     Analog of strchr (<string.h>)
+    \param[in] str - string
+    \param[in] sym - target symbol which we want to be found
+    \return pointer to first found target symbol
 */
 char *str_chr( const char *str, int sym );
 
 /*!
     Analog of strcmp (<string.h>)
+    \param[in] str1 - first string
+    \param[in] str2 - second string
+    \return difference between two strings
 */
 int str_cmp( const char *str1, const char *str2 );
 
 /*!
     Analog of strcat (<string.h>)
+    \param[out] dest - destination string
+    \param[in] src - source string
 */
 char *str_cat( char *dest, const char *src );
