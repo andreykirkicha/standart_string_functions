aac614629d87f59171afc58e4f03eb8f4bc9f029
andreykirkicha <kirkicha.andrey@mail.ru>
2022-08-29 10:41:16 +0300

    #3
    Добавлена документация

--- string_functions.hpp
+++ string_functions.hpp
@@ -1,5 +1,24 @@
+/*!
+    Analog of strlen (<string.h>)
+*/
 int str_len( const char *str );
+
+/*!
+    Analog of strcpy (<string.h>)
+*/
 char *str_cpy( char *dest, const char *src );
+
+/*!
+    Analog of strchr (<string.h>)
+*/
 char *str_chr( const char *str, int sym );
+
+/*!
+    Analog of strcmp (<string.h>)
+*/
 int str_cmp( const char *str1, const char *str2 );
+
+/*!
+    Analog of strcat (<string.h>)
+*/
 char *str_cat( char *dest, const char *src );
